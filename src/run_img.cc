6aae02acb85116fe726876c3304d379a61f92ab0
yazanmaalla <yazan.a.maalla@gmail.com>
2024-08-18 16:31:15 +0300

    save PLY mappoint

--- src/run_img.cc
+++ src/run_img.cc
@@ -1,8 +1,9 @@
 #include "util/image_util.h"
-
 #include "pangolin_viewer/viewer.h"
-
 #include "stella_vslam/system.h"
+#include "stella_vslam/data/frame.h"
+#include "stella_vslam/data/landmark.h"
+#include "stella_vslam/data/map_database.h"
 #include "stella_vslam/config.h"
 #include "stella_vslam/camera/base.h"
 #include "stella_vslam/util/yaml.h"
@@ -22,7 +23,6 @@
 namespace fs = ghc::filesystem;
 
 #include <pangolin/pangolin.h>
-
 #include <vector>
 #include <string>
 struct Point3D {
@@ -42,7 +42,6 @@ void WritePointCloudToPLY(const std::string& filename, const std::vector<Point3D
     ofs << "property float y\n";
     ofs << "property float z\n";
     ofs << "end_header\n";
-
     // Write point data
     for (const auto& point : points) {
         ofs << point.x << " " << point.y << " " << point.z << "\n";
@@ -52,28 +51,13 @@ void WritePointCloudToPLY(const std::string& filename, const std::vector<Point3D
 }
 
 
-
-
 int mono_tracking(const std::shared_ptr<stella_vslam::system>& slam,
                   const std::shared_ptr<stella_vslam::config>& cfg,
-                  const std::string& image_dir_path,
-                  const std::string& mask_img_path,
-                  const unsigned int frame_skip,
-                  const bool no_sleep,
-                  const bool wait_loop_ba,
-                  const bool auto_term,
-                  const std::string& eval_log_dir,
-                  const std::string& map_db_path,
-                  const double start_timestamp,
-                  const std::string& viewer_string) {
-    // load the mask image
-    const cv::Mat mask = mask_img_path.empty() ? cv::Mat{} : cv::imread(mask_img_path, cv::IMREAD_GRAYSCALE);
+                  const std::string& image_dir_path
+                  ) {
 
     const image_sequence sequence(image_dir_path);
     const auto frames = sequence.get_frames();
-
-    // create a viewer object
-
 
     std::shared_ptr<pangolin_viewer::viewer> viewer;
     
@@ -85,38 +69,27 @@ int mono_tracking(const std::shared_ptr<stella_vslam::system>& slam,
     
     std::vector<double> track_times;
     track_times.reserve(frames.size());
-    double timestamp = start_timestamp;
-
-    // run slam in another thread
+    double timestamp;
+    
     std::thread thread([&]() {
         for (unsigned int i = 0; i < frames.size(); ++i) {
 
-            // wait loop BA 
-            if (wait_loop_ba) {
-                while (slam->loop_BA_is_running() || !slam->mapping_module_is_enabled()) {
-                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-                }
-            }
 
             const auto& frame = frames.at(i);
             const auto img = cv::imread(frame.img_path_, cv::IMREAD_UNCHANGED);
-
             const auto tp_1 = std::chrono::steady_clock::now();
 
-            if (!img.empty() && (i % frame_skip == 0)) {
-                // input the current frame and estimate the camera pose
-                slam->feed_monocular_frame(img, timestamp, mask);
+            
+            if (!img.empty()) {
+                cv::Mat emptyMat;
+                slam->feed_monocular_frame(img, timestamp , emptyMat );
             }
 
             const auto tp_2 = std::chrono::steady_clock::now();
 
             const auto track_time = std::chrono::duration_cast<std::chrono::duration<double>>(tp_2 - tp_1).count();
-            if (i % frame_skip == 0) {
-                track_times.push_back(track_time);
-            }
+                if ( i < frames.size() - 1) {
 
-            // wait until the timestamp of the next frame
-            if (!no_sleep && i < frames.size() - 1) {
                 const auto wait_time = 1.0 / slam->get_camera()->fps_ - track_time;
                 if (0.0 < wait_time) {
                     std::this_thread::sleep_for(std::chrono::microseconds(static_cast<unsigned int>(wait_time * 1e6)));
@@ -124,7 +97,6 @@ int mono_tracking(const std::shared_ptr<stella_vslam::system>& slam,
             }
 
             timestamp += 1.0 / slam->get_camera()->fps_;
-
 
             // if termination of slam is requested 
             if (slam->terminate_is_requested()) {
@@ -137,13 +109,6 @@ int mono_tracking(const std::shared_ptr<stella_vslam::system>& slam,
         while (slam->loop_BA_is_running()) {
             std::this_thread::sleep_for(std::chrono::microseconds(5000));
         }
-
-        // automatically close the viewer
-        if (auto_term) {
-            if (viewer_string == "pangolin_viewer") {
-                viewer->request_terminate();
-            }
-        }
     });
         viewer->run();
     
@@ -154,30 +119,9 @@ int mono_tracking(const std::shared_ptr<stella_vslam::system>& slam,
     slam->shutdown();
 
     
-        // output the trajectories for evaluation
+        //  trajectories  
         slam->save_frame_trajectory( "./frame_trajectory.txt", "TUM");
-        slam->save_keyframe_trajectory("./keyframe_trajectory.txt", "TUM");
-        // output the tracking times for evaluation
-        std::ofstream ofs("./track_times.txt", std::ios::out);
-        if (ofs.is_open()) {
-            for (const auto track_time : track_times) {
-                ofs << track_time << std::endl;
-            }
-            ofs.close();
-        }
-    
-
-    std::sort(track_times.begin(), track_times.end());
-    const auto total_track_time = std::accumulate(track_times.begin(), track_times.end(), 0.0);
-    std::cout << "median tracking time: " << track_times.at(track_times.size() / 2) << "[s]" << std::endl;
-    std::cout << "mean tracking time: " << total_track_time / track_times.size() << "[s]" << std::endl;
-
-    if (!map_db_path.empty()) {
-        if (!slam->save_map_database(map_db_path)) {
-            return EXIT_FAILURE;
-        }
-    }
-
+        // slam->save_keyframe_trajectory("./keyframe_trajectory.txt", "TUM");
 
 
     return EXIT_SUCCESS;
@@ -194,19 +138,8 @@ int main(int argc, char* argv[]) {
     auto vocab_file_path = op.add<popl::Value<std::string>>("v", "vocab", "vocabulary file path");
     auto img_dir_path = op.add<popl::Value<std::string>>("d", "img-dir", "directory path which contains images");
     auto config_file_path = op.add<popl::Value<std::string>>("c", "config", "config file path");
-    auto mask_img_path = op.add<popl::Value<std::string>>("", "mask", "mask image path", "");
-    auto frame_skip = op.add<popl::Value<unsigned int>>("", "frame-skip", "interval of frame skip", 1);
-    auto no_sleep = op.add<popl::Switch>("", "no-sleep", "not wait for next frame in real time");
-    auto wait_loop_ba = op.add<popl::Switch>("", "wait-loop-ba", "wait until the loop BA is finished");
-    auto auto_term = op.add<popl::Switch>("", "auto-term", "automatically terminate the viewer");
     auto log_level = op.add<popl::Value<std::string>>("", "log-level", "log level", "info");
-    auto eval_log_dir = op.add<popl::Value<std::string>>("", "eval-log-dir", "store trajectory and tracking times at this path (Specify the directory where it exists.)", "");
-    auto map_db_path_in = op.add<popl::Value<std::string>>("i", "map-db-in", "load a map from this path", "");
-    auto map_db_path_out = op.add<popl::Value<std::string>>("o", "map-db-out", "store a map database at this path after slam", "");
-    auto disable_mapping = op.add<popl::Switch>("", "disable-mapping", "disable mapping");
-    auto temporal_mapping = op.add<popl::Switch>("", "temporal-mapping", "enable temporal mapping");
-    auto start_timestamp = op.add<popl::Value<double>>("t", "start-timestamp", "timestamp of the start of the video capture");
-    auto viewer = op.add<popl::Value<std::string>>("", "viewer", "viewer [iridescence_viewer, pangolin_viewer, socket_publisher, none]");
+
     try {
         op.parse(argc, argv);
     }
@@ -235,40 +168,7 @@ int main(int argc, char* argv[]) {
         std::cerr << op << std::endl;
         return EXIT_FAILURE;
     }
-
-    // viewer
-    std::string viewer_string;
-
-
-
-
-    // if (viewer->is_set()) {
-        // viewer_string = viewer->value();
-        // if (viewer_string != "pangolin_viewer"
-            // && viewer_string != "none") {
-            // std::cerr << "invalid arguments (--viewer)" << std::endl
-                    //   << std::endl
-                    //   << op << std::endl;
-            // return EXIT_FAILURE;
-        // }
-// #ifndef HAVE_PANGOLIN_VIEWER
-//         if (viewer_string == "pangolin_viewer") {
-//             std::cerr << "pangolin_viewer not linked" << std::endl
-//                       << std::endl
-//                       << op << std::endl;
-//             return EXIT_FAILURE;
-//         }
-// #endif
-
-//     }
-//     else {
-// #ifdef HAVE_IRIDESCENCE_VIEWER
-//         viewer_string = "iridescence_viewer";
-// #elif defined(HAVE_PANGOLIN_VIEWER)
-        viewer_string = "pangolin_viewer";
-// #endif
-//     }
-
+    
     // setup logger
     spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] %^[%L] %v%$");
     spdlog::set_level(spdlog::level::from_str(log_level->value()));
@@ -287,78 +187,39 @@ int main(int argc, char* argv[]) {
     ProfilerStart("slam.prof");
 #endif
 
-    // You cannot get timestamps of images with this input format.
-    // It is recommended to specify the timestamp when the video recording was started in Unix time.
-    // If not specified, the current system time is used instead.
     double timestamp = 0.0;
-    if (!start_timestamp->is_set()) {
-        std::cerr << "--start-timestamp is not set. using system timestamp." << std::endl;
-        if (no_sleep->is_set()) {
-            std::cerr << "If --no-sleep is set without --start-timestamp, timestamps may overlap between multiple runs." << std::endl;
-        }
-        std::chrono::system_clock::time_point start_time_system = std::chrono::system_clock::now();
-        timestamp = std::chrono::duration_cast<std::chrono::duration<double>>(start_time_system.time_since_epoch()).count();
-    }
-    else {
-        timestamp = start_timestamp->value();
-    }
-
-    // build a slam system
+    std::cerr << "--start-timestamp is not set. using system timestamp." << std::endl;
+    
+    std::chrono::system_clock::time_point start_time_system = std::chrono::system_clock::now();
+    timestamp = std::chrono::duration_cast<std::chrono::duration<double>>(start_time_system.time_since_epoch()).count();
+   
+    // build slam system
     auto slam = std::make_shared<stella_vslam::system>(cfg, vocab_file_path->value());
     bool need_initialize = true;
-    if (map_db_path_in->is_set()) {
-        need_initialize = false;
-        const auto path = fs::path(map_db_path_in->value());
-        if (path.extension() == ".yaml") {
-            YAML::Node node = YAML::LoadFile(path);
-            for (const auto& map_path : node["maps"].as<std::vector<std::string>>()) {
-                if (!slam->load_map_database(path.parent_path() / map_path)) {
-                    return EXIT_FAILURE;
-                }
-            }
-        }
-        else {
-            if (!slam->load_map_database(path)) {
-                return EXIT_FAILURE;
-            }
-        }
-    }
     slam->startup(need_initialize);
-    if (disable_mapping->is_set()) {
-        slam->disable_mapping_module();
-    }
-    else if (temporal_mapping->is_set()) {
-        slam->enable_temporal_mapping();
-        slam->disable_loop_detector();
-    }
 
     // run tracking
     int ret;
     if (slam->get_camera()->setup_type_ == stella_vslam::camera::setup_type_t::Monocular) {
         ret = mono_tracking(slam,
                             cfg,
-                            img_dir_path->value(),
-                            mask_img_path->value(),
-                            frame_skip->value(),
-                            no_sleep->is_set(),
-                            wait_loop_ba->is_set(),
-                            auto_term->is_set(),
-                            eval_log_dir->value(),
-                            map_db_path_out->value(),
-                            timestamp,
-                            viewer_string);
+                            img_dir_path->value()
+                            );
     }
     else {
         throw std::runtime_error("Invalid setup type: " + slam->get_camera()->get_setup_type_string());
     }
 
-
-
-// auto pointcloud =slam->map_db_;
-
-#ifdef USE_GOOGLE_PERFTOOLS
-    ProfilerStop();
-#endif
+auto map_db = slam->get_map_db();
+std::vector<Point3D> points;
+if (map_db) {
+    const auto landmarks = map_db->get_all_landmarks();
+    for (const auto& landmark : landmarks) {
+        const auto& pos = landmark->get_pos_in_world();
+        points.push_back({pos(0), pos(1), pos(2)});
+    }
+}
+WritePointCloudToPLY("./pointcloud.ply", points);
 
     return ret;
 }
